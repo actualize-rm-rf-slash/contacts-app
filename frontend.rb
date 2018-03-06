c22e57e707e05bfb3195d2c137d6461e3f9090fc
Peter Jang <peter.x.jang@gmail.com>
2018-03-05 21:18:07 -0600

    Add options in frontend

--- frontend.rb
+++ frontend.rb
@@ -1,6 +1,17 @@
 require "unirest"
 
 system "clear"
-response = Unirest.get("http://localhost:3000/show_one_contact_url")
-contact = response.body
-puts JSON.pretty_generate(contact)
+puts "Choose an option:"
+puts "[1] Show one contact"
+puts "[2] Show all contacts"
+
+input_option = gets.chomp
+if input_option == "1"
+  response = Unirest.get("http://localhost:3000/show_one_contact_url")
+  contact = response.body
+  puts JSON.pretty_generate(contact)
+elsif input_option == "2"
+  response = Unirest.get("http://localhost:3000/show_all_contacts_url")
+  contacts = response.body
+  puts JSON.pretty_generate(contacts)
+end
