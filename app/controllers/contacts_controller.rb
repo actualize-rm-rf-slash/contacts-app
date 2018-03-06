7ca0d5052c9ef721ee1c1bb5c3f97b1a6aa35d05
Peter Jang <peter.x.jang@gmail.com>
2018-03-05 21:15:53 -0600

    Refactor controller methods to use .as_json

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,24 +1,11 @@
 class ContactsController < ApplicationController
   def show_one_contact_method
     contact = Contact.first
-    render json: {
-      first_name: contact.first_name,
-      last_name: contact.last_name,
-      email: contact.email,
-      phone_number: contact.phone_number
-    }
+    render json: contact.as_json
   end
 
   def show_all_contacts_method
     contacts = Contact.all
-    contact_hashes = contacts.map do |contact|
-      {
-        first_name: contact.first_name,
-        last_name: contact.last_name,
-        email: contact.email,
-        phone_number: contact.phone_number
-      }
-    end
-    render json: contact_hashes
+    render json: contacts.as_json
   end
 end
