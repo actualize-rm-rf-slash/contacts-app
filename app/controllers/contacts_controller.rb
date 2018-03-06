e7833009fab550ffcf74530ef111f165c9624875
Peter Jang <peter.x.jang@gmail.com>
2018-03-05 20:49:07 -0600

    Add route to show first contact info

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,2 +1,11 @@
 class ContactsController < ApplicationController
+  def show_one_contact_method
+    contact = Contact.first
+    render json: {
+      first_name: contact.first_name,
+      last_name: contact.last_name,
+      email: contact.email,
+      phone_number: contact.phone_number
+    }
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "/show_one_contact_url" => "contacts#show_one_contact_method"
 end
