678683c73bafcb52015234e5fdf006cd774cf514
Peter Jang <peter.x.jang@gmail.com>
2018-03-12 19:38:37 -0500

    Add RESTful API actions for contacts

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :v1 do
-    get "/show_one_contact_url" => "contacts#show_one_contact_method"
-    get "/show_all_contacts_url" => "contacts#show_all_contacts_method"
+    get "/contacts" => "contacts#index"
+    post "/contacts" => "contacts#create"
+    get "/contacts/:id" => "contacts#show"
+    patch "/contacts/:id" => "contacts#update"
+    delete "/contacts/:id" => "contacts#destroy"
   end
 end
--- frontend.rb
+++ frontend.rb
@@ -2,16 +2,49 @@ require "unirest"
 
 system "clear"
 puts "Choose an option:"
-puts "[1] Show one contact"
-puts "[2] Show all contacts"
+puts "[1] Show all contacts"
+puts "[2] Create a contact"
+puts "[3] Show one contact"
+puts "[4] Update a contact"
+puts "[5] Delete a contacts"
 
 input_option = gets.chomp
 if input_option == "1"
-  response = Unirest.get("http://localhost:3000/v1/show_one_contact_url")
-  contact = response.body
-  puts JSON.pretty_generate(contact)
-elsif input_option == "2"
-  response = Unirest.get("http://localhost:3000/v1/show_all_contacts_url")
+  response = Unirest.get("http://localhost:3000/v1/contacts")
   contacts = response.body
   puts JSON.pretty_generate(contacts)
+elsif input_option == "2"
+  params = {
+    first_name: "Peter",
+    last_name: "Jang",
+    email: "peter@email.com",
+    phone_number: "232343243"
+  }
+  response = Unirest.post("http://localhost:3000/v1/contacts", parameters: params)
+  contact = response.body
+  puts JSON.pretty_generate(contact)
+elsif input_option == "3"
+  print "Enter a contact id: "
+  contact_id = gets.chomp
+  response = Unirest.get("http://localhost:3000/v1/contacts/#{contact_id}")
+  contact = response.body
+  puts JSON.pretty_generate(contact)
+elsif input_option == "4"
+  print "Enter a contact id: "
+  contact_id = gets.chomp
+  params = {
+    first_name: "UpdatedFirstName",
+    last_name: "UpdatedLastName",
+    email: "updatedemail@email.com",
+    phone_number: "0000000"
+  }
+  response = Unirest.patch("http://localhost:3000/v1/contacts/#{contact_id}", parameters: params)
+  contact = response.body
+  puts JSON.pretty_generate(contact)
+elsif input_option == "5"
+  print "Enter a contact id: "
+  contact_id = gets.chomp
+  response = Unirest.delete("http://localhost:3000/v1/contacts/#{contact_id}")
+  body = response.body
+  puts JSON.pretty_generate(body)
 end
