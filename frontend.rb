5caeedef6efdfb8e0e13e3e167d23ccf0e091d20
Peter Jang <peter.x.jang@gmail.com>
2018-03-20 18:23:38 -0500

    Add signup option (part 1 of cheat sheet)

--- frontend.rb
+++ frontend.rb
@@ -2,6 +2,32 @@ require "unirest"
 
 system "clear"
 puts "Choose an option:"
+puts "[1] Signup"
+puts "[2] Login"
+puts "[3] Logout"
+
+input_option = gets.chomp
+if input_option == "1"
+  response = Unirest.post(
+    "http://localhost:3000/v1/users",
+    parameters: {
+      name: "Test",
+      email: "test@email.com",
+      password: "password",
+      password_confirmation: "password"
+    }
+  )
+  p response.body
+elsif input_option == "2"
+elsif input_option == "3"
+end
+
+puts "Press enter to continue"
+gets.chomp
+
+
+system "clear"
+puts "Choose an option:"
 puts "[1] Show all contacts"
 puts "  [1.1] Search contacts that contain 'an'"
 puts "[2] Create a contact"
